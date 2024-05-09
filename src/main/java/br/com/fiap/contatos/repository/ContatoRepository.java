6601f13c0a624f5edecca5b832e189a11c9cfc2d
Arttutu <arthurtt08@gmail.com>
2024-05-09 10:24:22 -0300

    criando classe service e a interface repositor usando jpql

---
+++ src/main/java/br/com/fiap/contatos/repository/ContatoRepository.java
@@ -0,0 +1,11 @@
+package br.com.fiap.contatos.repository;
+import br.com.fiap.contatos.model.Contato;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDate;
+import java.util.List;
+
+public interface ContatoRepository extends JpaRepository<Contato, Long> {
+
+    public Contato findByNome(String nome);
+    public List<Contato> findBydataNascimentoBetween (LocalDate dataInicial, LocalDate dataFinal);
+}
